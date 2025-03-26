04453c654b10364f5f02bd5b7be440a13e249552
BalaiDuckman2 <raf29450@gmail.com>
2025-03-26 17:36:57 +0100

    dd

--- html/v3/script_v3.js
+++ html/v3/script_v3.js
@@ -1,134 +1,160 @@
-let debutv3 = 0;  // Déclaration globale de 'debutv3'
+let debut2 = 0;
 const perPage = 25;
-let fin = perPage;
+let fin2 = perPage;
 let countri = Object.values(Country.all_countrie);
 
-// Fonction pour définir un cookie
 function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-    console.log("Cookie défini:", document.cookie); 
+  let expires = "";
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+  console.log("Cookie défin2i:", document.cookie);
 }
 
-// Fonction pour obtenir un cookie
 function getCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i].trim();
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i].trim();
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 
-// Récupérer la page sauvegardée dans le cookie
 let savedPage = getCookie("currentPage");
 if (savedPage !== null && !isNaN(parseInt(savedPage))) {
-    debutv3 = parseInt(savedPage) * perPage; // Affectation de la valeur à 'debutv3'
-    fin = debutv3 + perPage;
-    console.log("Page chargée depuis le cookie:", savedPage);
+  debut2 = parseInt(savedPage) * perPage;
+  fin2 = debut2 + perPage;
+  console.log("Page chargée depuis le cookie:", savedPage);
 } else {
-    console.log("Aucun cookie trouvé, début à 0"); 
+  console.log("Aucun cookie trouvé, début à 0");
 }
 
 function displayCountries() {
-    let tableBody = $("#country-list");
-    tableBody.empty();
-    let paginatedCountries = countri.slice(debutv3, fin);
+  let tableBody = $("#country-list");
+  tableBody.empty();
+  let paginatedCountries = countri.slice(debut2, fin2);
 
-    paginatedCountries.forEach(element => {
-        let row = `<tr>
+  paginatedCountries.forEach((element) => {
+    let row = `<tr>
             <td>${element.nom}</td>
             <td>${element.population}</td>
             <td>${element.superficie}</td>
             <td>${element.getPopDensity()}</td>
             <td>${element.continent}</td>
             <td><img src="${element.drapeau}" alt="Drapeau"></td>
+            <td style='display:none;'>${element.alpha3}</td>
+
         </tr>`;
-        tableBody.append(row);
+    tableBody.append(row);
+  });
+  let currentPage = Math.floor(debut2 / perPage) + 1;
+  $("#page-number").text(currentPage);
+}
+
+function resetEventListeners() {
+  $("tr")
+    .off("click")
+    .on("click", function () {
+      let alpha3 = $(this).find("td").last().text();
+      info(alpha3);
     });
 
-    let currentPage = Math.floor(debutv3 / perPage) + 1;
-    $("#page-number").text(currentPage);
+  $("img")
+    .off("click")
+    .on("click", function (event) {
+      event.stopPropagation();
+      let drapeau = $(this).attr("src");
+      imageGrand(drapeau);
+    });
 }
 
 $(document).ready(function () {
-    displayCountries();
+  displayCountries();
+  resetEventListeners();
 
-    $("#next-button").click(function () {
-        if (fin < countri.length) {
-            debutv3 += perPage;
-            fin += perPage;
-            setCookie("currentPage", Math.floor(debutv3 / perPage), 7); 
-            displayCountries();
-        }
-    });
+  $("#next-button").click(function () {
+    if (fin2 < countri.length) {
+      debut2 += perPage;
+      fin2 += perPage;
+      setCookie("currentPage", Math.floor(debut2 / perPage), 7);
+      displayCountries();
+      resetEventListeners();
+    }
+  });
 
-    $("#prev-button").click(function () {
-        if (debutv3 > 0) {
-            debutv3 -= perPage;
-            fin -= perPage;
-            setCookie("currentPage", Math.floor(debutv3 / perPage), 7); 
-            displayCountries();
-        }
-    });
-});
+  $("#prev-button").click(function () {
+    if (debut2 > 0) {
+      debut2 -= perPage;
+      fin2 -= perPage;
+      setCookie("currentPage", Math.floor(debut2 / perPage), 7);
+      displayCountries();
+      resetEventListeners();
+    }
+  });
 
-
-$("tr").on("click", function() {
+  $("tr").on("click", function () {
     let alpha3 = $(this).find("td").last().text();
-    
     info(alpha3);
-});
+  });
 
-$("img").on("click", function() {
+  $("img").on("click", function () {
     event.stopPropagation();
     let drapeau = $(this).attr("src");
-    
-    imageGrand(drapeau);
 
+    imageGrand(drapeau);
+  });
 });
 
-
-
 function info(alpha3) {
-    $("#image").remove();  
-    let country = Country.all_countrie[alpha3];
-    let infoDiv = $("#info");
-    if (infoDiv.length === 0) {
-        infoDiv = $("<div id='info' style='position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border: 1px solid black;'></div>").insertBefore("table");
-    }
-    infoDiv.html(
-        "<p>Country: " + (country.nom || "Nom inconnu") + "</p>" +
-        "<p>Population: " + (country.population || "Inconnue") + "</p>" +
-        "<p>Superficie: " + (country.superficie || "Inconnue") + "</p>" +
-        "<p>Population Density: " + (country.getPopDensity ? country.getPopDensity() : "Inconnue") + "</p>" +
-        "<p>Continent: " + (country.continent || "Inconnu") + "</p>" +
-        "<button id='fermer'>fermer</button>"
-    );
+  $("#image").remove();
+  let country = Country.all_countrie[alpha3];
+  let infoDiv = $("#info");
+  if (infoDiv.length === 0) {
+    infoDiv = $(
+      "<div id='info' style='position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border: 1px solid black;'></div>"
+    ).insertBefore("table");
+  }
+  infoDiv.html(
+    "<p>Country: " +
+      country.nom +
+      "</p>" +
+      "<p>Population: " +
+      country.population +
+      "</p>" +
+      "<p>Superficie: " +
+      country.superficie +
+      "</p>" +
+      "<p>Population Density: " +
+      country.getPopDensity() +
+      "</p>" +
+      "<p>Continent: " +
+      country.continent +
+      "</p>" +
+      "<button id='fermer'>fermer</button>"
+  );
 
-    $("#fermer").on("click", function() {
-        $("#info").remove();        
-    });
+  $("#fermer").on("click", function () {
+    $("#info").remove();
+  });
 }
 
-function imageGrand(drapeau){
-    $("#info").remove();
-    let imageDiv = $("#image");
-    if (imageDiv.length === 0) {
-        imageDiv = $("<div id='image' style='position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border: 1px solid black;'></div>").insertBefore("table");
-    }
-    imageDiv.html(
-        "<img src=" + drapeau + "></img>"+
-        "<button id='fermer2'>fermer</button>"
-    );
+function imageGrand(drapeau) {
+  $("#info").remove();
+  let imageDiv = $("#image");
+  if (imageDiv.length === 0) {
+    imageDiv = $(
+      "<div id='image' style='position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border: 1px solid black;'></div>"
+    ).insertBefore("table");
+  }
+  imageDiv.html(
+    "<img src=" + drapeau + "></img>" + "<button id='fermer2'>fermer</button>"
+  );
 
-    $("#fermer2").on("click", function() {
-        $("#image").remove();        
-    });
+  $("#fermer2").on("click", function () {
+    $("#image").remove();
+  });
 }
